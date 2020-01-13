c71ad5b2c9d8349b6f687168fdcde6f920c30705
Kai Yamada <gpioblink@aol.com>
2020-01-13 19:45:52 +0900

    add update func

--- editor/infrastructure/lyric/memory.go
+++ editor/infrastructure/lyric/memory.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gpioblink/go-auto-clean-fme-editor/editor/domain/lyric"
 )
 
-var ErrIndexOutOfBounds = errors.New("index out of bounds")
+var ErrIndexNotFound = errors.New("index not found")
 
 type MemoryRepository struct {
 	lyrics []lyric.Lyric
@@ -21,11 +21,19 @@ func (m *MemoryRepository) Save(lyricToSave *lyric.Lyric) error {
 }
 
 func (m *MemoryRepository) Update(lyricToUpdate *lyric.Lyric, index int) error {
-	if len(m.lyrics) < index {
-		return ErrIndexOutOfBounds
+	if len(m.lyrics) <= index {
+		return ErrIndexNotFound
 	}
 	m.lyrics[index] = *lyricToUpdate
 	return nil
+}
+
+func (m MemoryRepository) ByIndex(index int) (*lyric.Lyric, error) {
+	if len(m.lyrics) <= index {
+		return nil, ErrIndexNotFound
+	}
+
+	return &m.lyrics[index], nil
 }
 
 func (m *MemoryRepository) ListAllLyrics() ([]lyric.Lyric, error) {
--- editor/infrastructure/lyric/memory_test.go
+++ editor/infrastructure/lyric/memory_test.go
@@ -12,9 +12,29 @@ func TestMemoryRepository(t *testing.T) {
 
 	assertAllProducts(t, repo, []lyric_domain.Lyric{})
 
-	lyric1 := addLyric(t, repo)
+	lyric1 := createLyric(t)
+	addLyric(t, repo, &lyric1)
 
-	assertAllProducts(t, repo, []lyric_domain.Lyric{*lyric1})
+	assertAllProducts(t, repo, []lyric_domain.Lyric{lyric1})
+
+	lyric2 := createUpdatingLyric(t)
+	updateLyric(t, repo, &lyric2, 0, false)
+
+	assertAllProducts(t, repo, []lyric_domain.Lyric{lyric2})
+
+	addLyric(t, repo, &lyric1)
+	addLyric(t, repo, &lyric1)
+	addLyric(t, repo, &lyric1)
+	updateLyric(t, repo, &lyric2, 2, false)
+
+	assertAllProducts(t, repo, []lyric_domain.Lyric{lyric2, lyric1, lyric2, lyric1})
+
+	updateLyric(t, repo, &lyric2, 1234, true)
+
+	l := byIndex(t, repo, 0, false)
+	assert.EqualValues(t, lyric2, *l)
+
+	byIndex(t, repo, 4, true)
 }
 
 func assertAllProducts(t *testing.T, repo *lyric.MemoryRepository, expectedLyric []lyric_domain.Lyric) {
@@ -23,13 +43,31 @@ func assertAllProducts(t *testing.T, repo *lyric.MemoryRepository, expectedLyric
 	assert.EqualValues(t, expectedLyric, lyrics)
 }
 
-func addLyric(t *testing.T, repo *lyric.MemoryRepository) *lyric_domain.Lyric {
-	l := createLyric(t)
+func byIndex(t *testing.T, repo *lyric.MemoryRepository, index int, isErr bool) *lyric_domain.Lyric {
+	l, err := repo.ByIndex(index)
 
-	err := repo.Save(&l)
+	if isErr {
+		assert.Error(t, err)
+	} else {
+		assert.NoError(t, err)
+	}
+
+	return l
+}
+
+func addLyric(t *testing.T, repo *lyric.MemoryRepository, lyric *lyric_domain.Lyric) {
+	err := repo.Save(lyric)
 	assert.NoError(t, err)
+}
 
-	return &l
+func updateLyric(t *testing.T, repo *lyric.MemoryRepository, lyric *lyric_domain.Lyric, index int, isErr bool) {
+	err := repo.Update(lyric, index)
+
+	if isErr {
+		assert.Error(t, err)
+	} else {
+		assert.NoError(t, err)
+	}
 }
 
 func createLyric(t *testing.T) lyric_domain.Lyric {
@@ -63,6 +101,50 @@ func createLyric(t *testing.T) lyric_domain.Lyric {
 	assert.NoError(t, err)
 
 	lyricString := []lyric_domain.LyricChar{*char1, *char2, *char3, *char4}
+
+	testLyric, err := lyric_domain.NewLyric(*point, *colorPicker, lyricString)
+	assert.NoError(t, err)
+
+	return *testLyric
+}
+
+func createUpdatingLyric(t *testing.T) lyric_domain.Lyric {
+	point, err := lyric_domain.NewPoint(15, 28)
+	assert.NoError(t, err)
+
+	color1, err := lyric_domain.NewColor(112, 234, 113)
+	assert.NoError(t, err)
+
+	color2, err := lyric_domain.NewColor(112, 234, 113)
+	assert.NoError(t, err)
+
+	color3, err := lyric_domain.NewColor(112, 234, 113)
+	assert.NoError(t, err)
+
+	color4, err := lyric_domain.NewColor(112, 234, 113)
+	assert.NoError(t, err)
+
+	colorPicker, err := lyric_domain.NewColorPicker(*color1, *color2, *color3, *color4)
+
+	char1, err := lyric_domain.NewLyricChar("き", 16, "きみ")
+	assert.NoError(t, err)
+
+	char2, err := lyric_domain.NewLyricChar("み", 23, "")
+	assert.NoError(t, err)
+
+	char3, err := lyric_domain.NewLyricChar("は", 23, "")
+	assert.NoError(t, err)
+
+	char4, err := lyric_domain.NewLyricChar("は", 23, "")
+	assert.NoError(t, err)
+
+	char5, err := lyric_domain.NewLyricChar("は", 23, "")
+	assert.NoError(t, err)
+
+	char6, err := lyric_domain.NewLyricChar("は", 23, "")
+	assert.NoError(t, err)
+
+	lyricString := []lyric_domain.LyricChar{*char1, *char2, *char3, *char4, *char5, *char6}
 
 	testLyric, err := lyric_domain.NewLyric(*point, *colorPicker, lyricString)
 	assert.NoError(t, err)
