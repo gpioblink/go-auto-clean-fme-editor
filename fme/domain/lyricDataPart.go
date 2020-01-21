dea97e32d6b7be03fb42fe2aefa1c5c11c4cc7e6
Kai Yamada <gpioblink@aol.com>
2020-01-21 11:27:00 +0900

    define structure of the lyric data part

--- fme/domain/lyricDataPart.go
+++ fme/domain/lyricDataPart.go
@@ -1,7 +1,36 @@
 package fme
 
 type LyricDataPart struct {
-
+	LyricHeader
+	LyricBody
+	LyricRuby
 }
 
+type LyricHeader struct {
+	colors        [15]uint16
+	lyricDataSize uint16
+	flag          uint16
+	x             uint16
+	y             uint16
+	colorSelectBC byte
+	colorSelectAC byte
+	colorSelectBO byte
+	colorSelectAO byte
+}
 
+type LyricBody struct {
+	lyricCount uint16
+	lyrics     []LyricChar
+}
+
+type LyricChar struct {
+	fontCode byte
+	char     [2]byte
+	width    uint16
+}
+
+type LyricRuby struct {
+	RubyCount               uint16
+	RelativeHorizontalPoint uint16
+	RubyChar                []byte
+}
